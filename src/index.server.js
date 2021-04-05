74690d80ddf44f5bdbd29eaef5c75aa8885b0050
cse-mkamble <cse.mkamble@gmail.com>
2021-04-05 17:17:23 +0530

    MongoDB use & create data.

--- src/index.server.js
+++ src/index.server.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const env = require('dotenv');
-
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 env.config();
+app.use(bodyParser());
+mongoose.connect(
+    'mongodb://localhost:27017/inbaazaar', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    }
+).then(() => {
+    console.log('Databases Connected.....');
+});
+
+// routes
+const authRoutes = require('./routes/auth');
+
+
+
+
+// API
+app.use('/api', authRoutes);
+
+
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}......`);
