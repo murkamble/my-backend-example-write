c61b33fee20a55262fa04e1e9c691e5539432f9d
cse-mkamble <cse.mkamble@gmail.com>
2021-04-06 12:12:57 +0530

    User signin & signup check api

--- src/controller/auth.js
+++ src/controller/auth.js
@@ -1,7 +1,7 @@
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
 
 exports.signup = (req, res) => {
-
     User.findOne({ email: req.body.email })
         .exec((error, user) => {
             if (user) {
@@ -9,10 +9,8 @@ exports.signup = (req, res) => {
                     message: 'User already registered.....'
                 });
             }
-
             const { firstName, lastName, email, password } = req.body;
             const _user = new User({ firstName, lastName, email, password, username: Math.random().toString() });
-
             _user.save((error, data) => {
                 if (error) {
                     return res.status(400).json({
@@ -26,5 +24,29 @@ exports.signup = (req, res) => {
                 }
             })
         })
+}
 
+exports.signin = (req, res) => {
+    User.findOne({ email: req.body.email })
+        .exec((error, user) => {
+            if (error) return res.status(400).json({ error });
+            if (user) {
+
+                if (user.authenticate(req.body.password)) {
+                    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+                    const { firstName, lastName, email, role, fullName } = user;
+                    res.status(200).json({
+                        token,
+                        user: { firstName, lastName, email, role, fullName }
+                    });
+                } else {
+                    return res.status(400).json({
+                        message: 'Invalid Password.....'
+                    })
+                }
+
+            } else {
+                return res.status(400).json({ message: 'Something went wrong.....' })
+            }
+        });
 }
